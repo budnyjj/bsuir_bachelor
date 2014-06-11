6e1fc774ca63f4b54387f94799388b490db3abf5
Pavel Anashkevich <anashkevichp@gmail.com>
2014-06-11 17:06:53 +0300

    BIBD, course work: database backup added

---
+++ discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/service/impl/BicyclesService.java
@@ -0,0 +1,41 @@
+package com.anash.bikeshop.service.impl;
+
+import com.anash.bikeshop.entity.Bicycles;
+import com.anash.bikeshop.repository.BicyclesRepo;
+import com.anash.bikeshop.service.BicyclesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BicyclesService implements BicyclesService {
+
+    @Autowired
+    private BicyclesRepo bicyclesRepo;
+
+    @Override
+    public void create(Bicycles bicycles) {
+        bicyclesRepo.saveAndFlush(bicycles);
+    }
+
+    @Override
+    public void update(Bicycles bicycles) {
+        bicyclesRepo.saveAndFlush(bicycles);
+    }
+
+    @Override
+    public void delete(Bicycles bicycles) {
+        bicyclesRepo.delete(bicycles);
+    }
+
+    @Override
+    public Bicycles getById(long id) {
+        return bicyclesRepo.findOne(id);
+    }
+
+    @Override
+    public List<Bicycles> getAll() {
+        return bicyclesRepo.findAll();
+    }
+}
