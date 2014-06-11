994c909e96aff0e87ec86ae6dc19b0d757021c48
Pavel Anashkevich <anashkevichp@gmail.com>
2014-06-11 17:18:19 +0300

    refacoring bug fixed

--- discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/service/impl/BicyclesServiceImpl.java
+++ discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/service/impl/BicyclesServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class BicyclesService implements BicyclesService {
+public class BicyclesServiceImpl implements BicyclesService {
 
     @Autowired
     private BicyclesRepo bicyclesRepo;
