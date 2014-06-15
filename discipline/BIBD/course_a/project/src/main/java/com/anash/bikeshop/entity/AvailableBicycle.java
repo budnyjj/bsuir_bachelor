a4035883e4b198b4d1b695c605453b8d8009b4f1
Pavel Anashkevich <anashkevichp@gmail.com>
2014-06-15 18:58:00 +0300

    BIBD, course work: reference id types bug fixed

--- discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/entity/AvailableBicycle.java
+++ discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/entity/AvailableBicycle.java
@@ -9,7 +9,7 @@ import java.io.Serializable;
 public class AvailableBicycle implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Integer id;
 
     @NotNull
     @OneToOne(cascade = CascadeType.ALL)
@@ -22,11 +22,11 @@ public class AvailableBicycle implements Serializable {
     @Column
     private Boolean availability;
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
