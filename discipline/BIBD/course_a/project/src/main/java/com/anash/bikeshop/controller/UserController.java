90a51e4f4f2911d0b3d29912fba8f5061a906263
budnyjj <budnyjj@gmail.com>
2014-06-15 19:30:49 +0300

    update user controller

--- discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/controller/UserController.java
+++ discipline/BIBD/course_a/project/src/main/java/com/anash/bikeshop/controller/UserController.java
@@ -4,6 +4,9 @@ import com.anash.bikeshop.entity.User;
 import com.anash.bikeshop.service.UserService;
 import com.anash.bikeshop.entity.Bicycle;
 import com.anash.bikeshop.service.BicycleService;
+import com.anash.bikeshop.entity.OrderArchive;
+import com.anash.bikeshop.service.OrderArchiveService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -23,15 +27,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private OrderArchiveService orderArchiveService;
+
     @RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
-    public ModelAndView bicycleDetail(@PathVariable Integer id) {
+    public ModelAndView userDetail(@PathVariable Integer id) {
         ModelAndView modelAndView = new ModelAndView("user_details");
+        List<OrderArchive> orders = orderArchiveService.getOrdersByUserId(id);
+        modelAndView.addObject("orders", orders);
+
+        List<Bicycle> bicycles = new ArrayList<>();
+        for (OrderArchive order: orders)
+            bicycles.add(order.getBicycle());
+
+        modelAndView.addObject("bicycles", bicycles);
 
         User user = userService.getById(id);
         modelAndView.addObject("user", user);
-
-        Bicycle bicycle = bicycleService.getById(id);
-        modelAndView.addObject("bicycle", bicycle);
 
         return modelAndView;
     }
