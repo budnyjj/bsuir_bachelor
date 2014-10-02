e60e1dd218ad4c860019f70f6fc1aa551a5fd765
Pavel Anashkevich <anashkevichp@gmail.com>
2014-10-02 15:57:29 +0300

    lab_10 SPO updated

--- discipline/SPO/lab_10/report/code/code.ps1
+++ discipline/SPO/lab_10/report/code/code.ps1
@@ -3,8 +3,8 @@
   This PowerShell script prints a list of running processes.
 
   .DESCRIPTION
-  This PowerShell script prints a list of running processes, sorting them by start time.
-  Color indicates the priority of a process.
+  This PowerShell script prints a list of running processes, sorting them by priority,
+  then by start time. Color indicates the priority of a process.
 
   .EXAMPLE
   .\processes.ps1
@@ -16,7 +16,7 @@
   Set-Variable MAX_PROCESS_NAME_LENGTH -option Constant -value 40
 
   $processes = Get-WmiObject -Class win32_process
-  $sorted_processes = $($processes | Sort-Object CreationDate -Descending)
+  $sorted_processes = $($processes | Sort-Object Priority, CreationDate)
 
   $process_number = 1
   $sorted_processes | foreach-object {
@@ -37,3 +37,35 @@
           $process_number += 1
       }
   }
+
+  ############## statistics #################
+
+  $priority_statistics = @()
+  $sorted_processes_count = $sorted_processes.GetUpperBound(0)
+
+  for ($i = 1; $i -le $sorted_processes_count; $i++) {
+
+      $count++;
+      if (($sorted_processes[$i].Priority -ne $old_priority)) {
+
+          $priority_statistics += $old_priority, $count
+          $count = 0
+
+      } elseif ($i -eq $sorted_processes_count) {
+          $priority_statistics += $old_priority, $($count+1)
+      }
+
+      $old_priority = $sorted_processes[$i].Priority
+  }
+
+  $processes_count = $sorted_processes.GetUpperBound(0)
+  $upperBound = $priority_statistics.GetUpperBound(0)
+
+  Write-Host ============ statistics ============
+  Write-Host Priority num `t Count `t Percentage
+  Write-Host ------------------------------------
+  for ($i = 2; $i -lt $upperBound; $i+=2) {
+      $percent = $($priority_statistics[$i+1] / $processes_count * 100)
+      Write-Host Priority "#" $priority_statistics[$i] `t $priority_statistics[$i+1] `t  $percent.ToString("F02")%
+  }
+  Write-Host ------------------------------------
