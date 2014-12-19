c7b44e4475b0f434800e62eb97d1c18732f2f2f8
Pavel Anashkevich <anashkevichp@gmail.com>
2014-12-19 23:58:02 +0300

    SPO #course_project_a added

---
+++ discipline/SPO/course_a/project/StatusBarApp/StatusBarApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  StatusBarApp
+//
+//  Created by Pavel Anashkevich on 12/11/14.
+//  Copyright (c) 2014 Pavel Anashkevich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
+
