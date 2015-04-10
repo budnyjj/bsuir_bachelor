838c5c0a90b2d75bc275a491a808ee1125a8a659
Pavel Anashkevich <anashkevichp@gmail.com>
2015-04-10 17:59:40 +0300

    SSP #6 project added

---
+++ discipline/SSP/lab_6/project/ComponentLibrary/ComponentLibrary/StringComponent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ComponentLibrary
+{
+    public partial class StringComponent : Component
+    {
+        public StringComponent()
+        {
+            InitializeComponent();
+        }
+
+        public StringComponent(IContainer container)
+        {
+            container.Add(this);
+            InitializeComponent();
+        }
+
+        public String HeaderOfString(String str, String delimiter)
+        {
+            if (str == null || str.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (delimiter == null || delimiter.Length == 0)
+                {
+                    return str;
+                }
+                else
+                {
+
+                    if (str.Contains(delimiter))
+                    {
+                        return str.Substring(0, str.IndexOf(delimiter));
+                    }
+                    else
+                    {
+                        return str;
+                    }
+                }
+            }
+        }
+
+        public String FooterOfString(String str, String delimiter)
+        {
+            if (str == null || str.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+                if (delimiter == null || delimiter.Length == 0)
+                {
+                    return "";
+                }
+                else
+                {
+                    if (str.Contains(delimiter))
+                    {
+                        return str.Substring(str.IndexOf(delimiter) + delimiter.Length);
+                    }
+                    else
+                    {
+                        return "";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ discipline/SSP/lab_6/project/TestComponent/TestComponent/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using ComponentLibrary;
+
+namespace TestComponent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void originalStringTextBox_TextChanged(object sender, EventArgs e)
+        {
+            processIncomeText(originalStringTextBox.Text, delimiterStringTextBox.Text);
+        }
+
+        private void delimiterStringTextBox_TextChanged(object sender, EventArgs e)
+        {
+            processIncomeText(originalStringTextBox.Text, delimiterStringTextBox.Text);
+        }
+
+        private void processIncomeText(String str, String delimiter)
+        {
+            StringComponent strComponent = new StringComponent();
+            headerStringTextBox.Text = strComponent.HeaderOfString(str, delimiter);
+            footerStringTextBox.Text = strComponent.FooterOfString(str, delimiter);
+        }
+    }
+}
