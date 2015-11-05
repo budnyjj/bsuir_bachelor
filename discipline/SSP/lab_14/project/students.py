bdee0f3f51b99cd640a72c984066194cdee51f13
Raman Budny <budnyjj@gmail.com>
2015-11-05 14:10:07 +0300

    SSP: update lab 14

--- discipline/SSP/lab_14/project/students.py
+++ discipline/SSP/lab_14/project/students.py
@@ -1,18 +1,70 @@
 #!/usr/bin/env python
 
+import argparse
 import functools
+import json
+import re
+
+class Constraints:
+    _PATTERN_NAME = '[A-Z][a-z]*'
+    _PATTERN_GROUP = '[0-9]{5,6}'
+
+    @classmethod
+    def isName(cls, s):
+        if re.fullmatch(cls._PATTERN_NAME, s):
+            return s
+        else:
+            raise ValueError(
+                'The name should be specified in format {}'.format(
+                    cls._PATTERN_NAME
+                )
+            )
+
+    @classmethod
+    def isGroupNumber(cls, s):
+        if re.fullmatch(cls._PATTERN_GROUP, s):
+            return s
+        else:
+            raise ValueError(
+                'The group should be specified in format {}'.format(
+                    cls._PATTERN_GROUP
+                )
+            )
 
 class Student:
-    fields = [
-        {'str': 'First name',   'repr': 'first_name', 'constraint': str},
-        {'str': 'Last name',    'repr': 'last_name',  'constraint': str},
-        {'str': 'Group number', 'repr': 'group',      'constraint': int}
-    ]
+    fields = (
+        {
+            'str': 'First name',
+            'repr': 'first_name',
+            'constraint': Constraints.isName
+        },
+        {
+            'str': 'Last name',
+            'repr': 'last_name',
+            'constraint': Constraints.isName
+        },
+        {
+            'str': 'Group number',
+            'repr': 'group',
+            'constraint': Constraints.isGroupNumber
+        }
+    )
 
-    def __init__(self, *field_values):
-        for field,field_value in zip(self.fields, field_values):
-            field_name = field['repr']
-            self.__dict__[field_name] = str(field_value)
+    @classmethod
+    def fromJSON(cls, j_str):
+        fields = json.loads(j_str)
+        return cls(**fields)
+
+    def __init__(self, **fields):
+        cstr_field_names = [f['repr'] for f in self.fields]
+        cstr_field_constraints = { f['repr']: f['constraint'] for f in self.fields }
+
+        for field_name,field_value in fields.items():
+            if field_name in cstr_field_names:
+                self.__dict__[field_name] =\
+                    cstr_field_constraints[field_name](field_value)
+            else:
+                raise ValueError('Cannot construct object from {}'.format(fields))
 
     def __str__(self):
         # determine length of maximal field name
@@ -26,6 +78,9 @@ class Student:
 
         return '\n'.join(res)
 
+    def toJSON(self):
+        return json.dumps(self.__dict__)
+
 def input_checked(prompt_msg, constraint):
     '''
     Asks user to input value and perform correctness check
@@ -38,21 +93,18 @@ def input_checked(prompt_msg, constraint):
         res = input_basic()
         try:
             checked = constraint(res)
-        except ValueError:
-            print('E: Could not convert "{}" to {}'.format(res, constraint))
-            print('   Please, try again')
-        except Exception:
-            print('E: Unknown error occured')
+        except Exception as e:
+            print('E: {}'.format(e))
             print('   Please, try again')
         else:
             return checked
 
 def input_object(cls):
-    fields = []
+    fields = {}
     for field in cls.fields:
         field_value = input_checked(field['str'], field['constraint'])
-        fields.append(field_value)
-    return cls(*fields)
+        fields[field['repr']] = field_value
+    return cls(**fields)
 
 def repl(handlers):
     while True:
@@ -64,6 +116,7 @@ def repl(handlers):
             print('{}. {}'.format(cur_idx, command[0]))
             cur_idx += 1
 
+        print()
         choice = input_checked('Your choice', int)
 
         if choice == 0:
@@ -71,6 +124,7 @@ def repl(handlers):
             print('Bye.')
             return
         elif choice >= 1 and choice <= len(commands):
+            print()
             # correct variant, so invoke handler
             handlers[choice-1][1]()
         else:
@@ -79,23 +133,16 @@ def repl(handlers):
 
         print()
 
+#
+# REPL handlers
+#
+
 def add_object(cls, lst):
     '''
     Input and append object to list
     '''
     obj = input_object(cls)
     lst.append(obj)
-
-def list_fields(key_field, lst):
-    '''
-    Show field value of each element in list
-    '''
-    if len(lst) == 0:
-        print('I: There is no objects in list')
-        return
-
-    for idx,obj in enumerate(lst):
-        print('{}. {}'.format(idx+1, getattr(obj, key_field)))
 
 def show_object(key_field, lst):
     '''
@@ -118,20 +165,57 @@ def show_object(key_field, lst):
             break
         if choice >= 1 and choice <= len(lst):
             # correct choice
+            print()
             print(lst[choice-1])
             break
         else:
             # incorrect choice
             print('E: Please, choose suitable menu item')
 
+def save(lst, fname):
+    if not fname:
+        print('E: Filename was not specified')
+        print('   Please, run this program with --file option')
+        return
 
+    with open(fname, 'w') as f:
+        for obj in lst:
+            f.write('{}\n'.format(obj.toJSON()))
+
+def load(cls, lst):
+    if not fname:
+        print('E: Filename was not specified')
+        print('   Please, run this program with --file option')
+        return
+
+    lst.clear()
+
+    with open(fname, 'r') as f:
+        for line in f:
+            obj = None
+            try:
+                obj = cls.fromJSON(line)
+            except Exception as e:
+                print('E: {}'.format(e))
+                print('   Invalid record format: {}'.format(line))
+                break
+            else:
+                lst.append(obj)
 
 
 if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        description = 'Student management demo program',
+    )
+    arg_parser.add_argument('--file', help='Students DB filename', required=False)
+    args = arg_parser.parse_args()
+    fname = args.file
+
     students = []
     commands = (
         ('Add student', functools.partial(add_object, Student, students)),
-        ('List students', functools.partial(list_fields, 'last_name', students)),
-        ('Show student', functools.partial(show_object, 'last_name', students)),
+        ('Show students', functools.partial(show_object, 'last_name', students)),
+        ('Save to file', functools.partial(save, students, fname)),
+        ('Load from file', functools.partial(load, Student, students)),
     )
     repl(commands)
